f970d942bdc8dff4fda82c1298ba6a9201fa3c1b
Ignacbo <ignacbo1@gmail.com>
2023-09-12 13:24:25 +0200

    LiczbaHolder class created

---
+++ ProjGithub/src/LiczbaHolder.java
@@ -0,0 +1,15 @@
+public class LiczbaHolder {
+    int i = 5;
+
+    public LiczbaHolder(int i) {
+        this.i = i;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+}

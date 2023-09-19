67d9c6fe24f15641e2aa0ed76e573d8d6ef5b341
Ignacbo <ignacbo1@gmail.com>
2023-09-19 11:58:45 +0200

    test, sout added

--- ProjGithub/src/Main.java
+++ ProjGithub/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         LiczbaHolder lh1 = new LiczbaHolder(12);
         LiczbaHolder lh2 = new LiczbaHolder(44);
+
+        System.out.println("test gita");
     }
 }
